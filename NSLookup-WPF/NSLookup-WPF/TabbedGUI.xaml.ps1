a279209511cda4225bfbb54eb7e7e2e9a2f807e8
Turner, Tony (T) <TTurner2@dow.com>
2018-02-19 10:02:12 -0600

    Adding Tabs

---
+++ NSLookup-WPF/NSLookup-WPF/TabbedGUI.xaml.ps1
@@ -0,0 +1,10 @@
+﻿function Load-Xaml {
+	[xml]$xaml = Get-Content -Path $PSScriptRoot\TabbedGUI.xaml
+	$manager = New-Object System.Xml.XmlNamespaceManager -ArgumentList $xaml.NameTable
+	$manager.AddNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml");
+	$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
+	[Windows.Markup.XamlReader]::Load($xamlReader)
+}
+
+$window = Load-Xaml
+$window.ShowDialog()
