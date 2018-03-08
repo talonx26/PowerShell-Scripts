0ad5a998bdb77617f136955dc6a54fd6edc26bad
Turner, Tony (T) <TTurner2@dow.com>
2018-03-08 09:21:18 -0600

    Will not feed just one page

--- GetLicenseScan/GetLicenseScan.ps1
+++ GetLicenseScan/GetLicenseScan.ps1
@@ -1,3 +1,6 @@
+$PSScriptRoot
+
+
 function get-LicenseScan
 {
     [CmdletBinding()]
@@ -7,80 +10,78 @@ function get-LicenseScan
     )
 
     Begin
-    {
+    { 
+        [void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+        $refs = @(
+            ".\BarcodeImaging.dll"
+
+        ) 
+
+
+        add-type -path $refs
         $deviceManager = New-Object -ComObject WIA.DeviceManager
         $device = $deviceManager.DeviceInfos.item(1).connect()
         # $wiaFormatPNG = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}"
         $wiaFormatJPEG = "{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}"
         $barcodes = New-Object System.Collections.ArrayList
         $imageProcess = new-object -ComObject WIA.ImageProcess
-
+        $resizeImageProcess = new-object -ComObject WIA.ImageProcess
     }
     Process
-    {
+    { 
         foreach ($item in $device.Items)
         { 
-            $item.Properties("Vertical Resolution") = 600
-            $item.Properties("Horizontal Resolution") = 600
-            $item.Properties("Format") = $wiaFormatJPEG
-            $item.Properties("Filename Extension") = "JPG"
+            $item.Properties("Vertical Resolution").Value = 600
+            $item.Properties("Horizontal Resolution").value = 600
+            
+            #  $item.Properties("Format") = $wiaFormatJPEG
+            #  $item.Properties("Filename Extension") = "JPG"
             $image = $item.Transfer($wiaFormatJPEG) 
         } 
+        $resizeImageProcess.Filters.Add($resizeImageProcess.FilterInfos.Item("Scale").FilterID)
         $imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
         $imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = $wiaFormatJPEG
-        #$imageProcess.Filters.Item(1).Properties.Item("Quality").Value = 5
-        $image = $imageProcess.Apply($image)
+        $imageProcess.Filters.Item(1).Properties.Item("Quality").Value = 50
+        $Saveimage = $imageProcess.Apply($image)
+        $resizeImageProcess.Filters.Item(1).Properties.Item("MaximumWidth").Value = [string]($image.Width * 2)
+        $resizeImageProcess.Filters.Item(1).Properties.Item("MaximumHeight").Value = [string]($image.height * 2)
+        $image = $resizeImageProcess.Apply($image)
+        
+        #$file = "C:\Scan\temp01.$($image.fileExtension)"
+        $Imageconverter = New-Object System.Drawing.ImageConverter
+        [System.Drawing.Bitmap]$Bitmap = $Imageconverter.ConvertFrom($image.FileData.BinaryData)
 
-
-
-        add-type -path ".\BarcodeImaging.dll"
-        $file = "C:\Scan\temp01.$($image.fileExtension)"
-        $image.SaveFile($file) 
-        $image = $null
-        $barcodes = New-Object System.Collections.ArrayList; [barcodeimaging]::ScanPage([ref]$barcodes, [System.Drawing.Bitmap]::FromFile($file), 600, 2, 1); 
+        #$image = $null
+        $barcodes = New-Object System.Collections.ArrayList; 
+        $Bitmap.Width
+        $Bitmap.Height
+        [barcodeimaging]::ScanPage([ref]$barcodes, $bitmap, 1000, 2, 1)
+        $filename = ""
+        $directory = ""
         foreach ($code in $barcodes)
         {
             if ($code.StartsWith("S"))
             {
                 $fileName = $code
+                "Filename  $filename"
             }
             else
             {
                 $directory = $code
+                "Dir : $directory"
                 if (!(test-path c:\scan\$directory))
                 {
                     new-item -ItemType Directory -Path c:\scan\$directory 
+
                 }
-            }
-            
+            }            
         }
-        move-item $file "c:\scan\$directory\$filename.jpg"
+
+        $saveimage.SaveFile("c:\scan\$directory\$filename.jpg") 
+
         
-        <# 
-        $refs = @(
-            "c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0\System.Drawing.dll"
-
-        ) 
-        #>
-
     }
     End
     {
     }
 }
-
-
-
-
-Add-Type -Path $refs
-
-<# 
-$index = 1
-do {
-    $index++
-} until (!(test-path "C:\Scan\test$index.$($image.fileExtension)"))
-$index
-
-    $image.SaveFile("C:\Scan\test$index.$($image.fileExtension)") 
-
- #>
