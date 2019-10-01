1dd306b6fa376d417906144bb2daf9df9f1d95d0
talonx26 <22669049+talonx26@users.noreply.github.com>
2019-10-01 15:54:24 -0500

    cleanup

--- SiemensLicenseScan/Get-SiemensLicenseID.ps1
+++ SiemensLicenseScan/Get-SiemensLicenseID.ps1
@@ -1,46 +1,40 @@
 #Requires -Version 3.0
-function Get-SiemensLicenseID
-{
+function Get-SiemensLicenseID {
 
     <# Synopsis
    #>
     [CmdletBinding()]
     [OutputType('PSCustomObject')]
     param (
-        [Parameter(Mandatory, 
+        [Parameter(Mandatory,
             ValueFromPipeline)]
         [object[]]$PartNumbers
     )
 
-    BEGIN
-    {
+    BEGIN {
         $refs = @(".\Dll\Microsoft.SharePoint.Client.dll", ".\dll\Microsoft.SharePoint.Client.Runtime.dll")
         add-type -Path $refs
         Add-Type -AssemblyName PresentationCore, PresentationFramework
-#set-location c:\scripts
-Add-Type -AssemblyName System.DirectoryServices.AccountManagement
-$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
-if (test-path "c:\scripts\creds\${env:username}_creds.xml")
-{
-    
-    $creds = Import-Clixml "c:\scripts\creds\${env:username}_creds.xml"
-    while (!($ds.ValidateCredentials($creds.UserName,$creds.GetNetworkCredential().password,[System.DirectoryServices.AccountManagement.ContextOptions]::Negotiate)))
-    {
-        $creds = Get-Credential -Message "Enter valid Sharepoint Online Credentials ex: fljpcnadmin@dow.com"
-        $creds | Export-Clixml "c:\scripts\creds\${env:username}_creds.xml"
-    }
+        #set-location c:\scripts
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
+        if (test-path "c:\scripts\creds\${env:username}_creds.xml") {
 
-}
-else
-{
-    $creds = Get-Credential -Message "Enter Sharepoint Online Credentials ex : fljpcnadmin@dow.com"
-    $creds | Export-Clixml "c:\scripts\creds\${env:username}_creds.xml"
-    while (!($ds.ValidateCredentials($creds.UserName,$creds.GetNetworkCredential().password,[System.DirectoryServices.AccountManagement.ContextOptions]::Negotiate)))
-    {
-        $creds = Get-Credential -Message "Enter valid Sharepoint Online Credentials ex: fljpcnadmin@dow.com"
-        $creds | Export-Clixml "c:\scripts\creds\${env:username}_creds.xml"
-    }
-}
+            $creds = Import-Clixml "c:\scripts\creds\${env:username}_creds.xml"
+            while (!($ds.ValidateCredentials($creds.UserName, $creds.GetNetworkCredential().password, [System.DirectoryServices.AccountManagement.ContextOptions]::Negotiate))) {
+                $creds = Get-Credential -Message "Enter valid Sharepoint Online Credentials ex: fljpcnadmin@dow.com"
+                $creds | Export-Clixml "c:\scripts\creds\${env:username}_creds.xml"
+            }
+
+        }
+        else {
+            $creds = Get-Credential -Message "Enter Sharepoint Online Credentials ex : fljpcnadmin@dow.com"
+            $creds | Export-Clixml "c:\scripts\creds\${env:username}_creds.xml"
+            while (!($ds.ValidateCredentials($creds.UserName, $creds.GetNetworkCredential().password, [System.DirectoryServices.AccountManagement.ContextOptions]::Negotiate))) {
+                $creds = Get-Credential -Message "Enter valid Sharepoint Online Credentials ex: fljpcnadmin@dow.com"
+                $creds | Export-Clixml "c:\scripts\creds\${env:username}_creds.xml"
+            }
+        }
         $ErrorActionPreference = "Continue"
         #Load SharePoint DLL's
         [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client")
@@ -49,46 +43,40 @@ else
         $context = New-Object Microsoft.SharePoint.Client.ClientContext($webURL)
         $credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($creds.UserName, $creds.Password)
         $context.Credentials = $credentials
-        $SoftwareIDs = @{}
+        $SoftwareIDs = @{ }
     }
-    Process
-    {
-        foreach ($partnumber in $partnumbers)
-        {
+    Process {
+        foreach ($partnumber in $partnumbers) {
             $web = $Context.Web
             $weblist = "MLKUPSiemensSoftware"
-            $Context.Load($web) 
-            $Context.ExecuteQuery() 
-            #If PartNumber.Text is Null then object is string is Barcode object  
-            if ($partnumber.Text -eq $null)
-            { $pn = $partnumber}
-            else
-            {
+            $Context.Load($web)
+            $Context.ExecuteQuery()
+            #If PartNumber.Text is Null then object is string is Barcode object
+            if ($null -eq $partnumber.Text)
+            { $pn = $partnumber }
+            else {
                 #PartNumber.Text is not Null so object is a Barcode Object
                 $pn = $partnumber.text
             }
             $list = $web.Lists.GetByTitle($weblist)
             $Query = New-Object Microsoft.SharePoint.Client.CamlQuery
-            
+
             $query.ViewXml = "<View Scope='RecursiveAll'><Query>,<Where><Eq><FieldRef Name='Part_x0020_Number'/><Value Type='Text'>$pn</Value></Eq></Where></Query></View>"
-            
+
             #$query.viewXml = [Microsoft.SharePoint.Client.CamlQuery]::CreateAllItemsQuery().Viewxml
-            $items = $list.GetItems($Query)  
+            $items = $list.GetItems($Query)
             $context.Load($items)
             $context.ExecuteQuery()
-            If ($items.count -eq 1)
-            {
-                If (!($SoftwareIDs.ContainsKey($pn)))
-                {
+            If ($items.count -eq 1) {
+                If (!($SoftwareIDs.ContainsKey($pn))) {
                     $SoftwareIDs.Add($pn, $items[0]["ID"])
                 }
             }
-            
+
         }
     }
 
-    END
-    {
+    END {
         #Used for cleanup. This code runs one time after all of the items specified via pipeline input are processed.
         Write-Verbose $SoftwareIDs -Verbose
         return $SoftwareIDs
--- SiemensLicenseScan/Get-SiemensLicenseScan.ps1
+++ SiemensLicenseScan/Get-SiemensLicenseScan.ps1
@@ -1,20 +1,19 @@
 #Requires -Version 3.0
-function Get-SiemensLicenseScan
-{
+function Get-SiemensLicenseScan {
 
     <#
 .SYNOPSIS
     Brief synopsis about the function.
- 
+
 .DESCRIPTION
     Detailed explanation of the purpose of this function.
- 
+
 .PARAMETER Param1
     The purpose of param1.
 
 .PARAMETER Param2
     The purpose of param2.
- 
+
 .EXAMPLE
      Get-SiemensLicenseScan -Param1 'Value1', 'Value2'
 
@@ -23,13 +22,13 @@ function Get-SiemensLicenseScan
 
 .EXAMPLE
      Get-SiemensLicenseScan -Param1 'Value1', 'Value2' -Param2 'Value'
- 
+
 .INPUTS
     String
- 
+
 .OUTPUTS
     PSCustomObject
- 
+
 .NOTES
     Author: Tony Turner
 #>
@@ -37,14 +36,13 @@ function Get-SiemensLicenseScan
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory = $false, 
+        [Parameter(Mandatory = $false,
             Position = 0)]
         $OutDirectory = "c:\scan"
     )
 
-    Begin
-    { 
-       
+    Begin {
+
         [void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
         add-type -Path .\Dll\Interop.WIA.dll
         $deviceManager = New-Object -ComObject WIA.DeviceManager
@@ -58,150 +56,132 @@ function Get-SiemensLicenseScan
         $MorePages = $true
     }
 
-    Process
-    { 
+    Process {
         Write-Verbose "Starting"
-        foreach ($item in $device.Items)
-        { 
+        foreach ($item in $device.Items) {
             $item.Properties("Vertical Resolution").Value = 600
             $item.Properties("Horizontal Resolution").value = 600
 
-         
-            do
-            {
-                Try { $image = $item.Transfer($wiaFormatJPEG)  }
-                Catch 
-                {
-                    if ($_.exception -ilike ("*no documents left*") -and $scan -eq 0)
-                    {
+
+            do {
+                Try { $image = $item.Transfer($wiaFormatJPEG) }
+                Catch {
+                    if ($_.exception -ilike ("*no documents left*") -and $scan -eq 0) {
                         #There have been no scans so ADF was empty to begin with
                         Write-host "No documents in Scanner. Please check and restart script"
                     }
-                    elseif (!($_.exception -ilike ("*no documents left*")) -and $scan -eq 0)
-                    {
-                        #This should catch error if Scanner doesn't start. 
+                    elseif (!($_.exception -ilike ("*no documents left*")) -and $scan -eq 0) {
+                        #This should catch error if Scanner doesn't start.
                         Write-host "If Scanner did not run try restarting scanner Scan : $Scan"
                     }
-                   
+
                     $MorePages = $false
-                  
-                   
+
+
                 }
-                
-                If ($MorePages)
-                {
-                   
+
+                If ($MorePages) {
+
                     $scan++
                     Write-Verbose "Scan Number $scan" -Verbose
                     #Image file for saving.  Smaller File for file size
                     $Saveimage = Resize-Image -Image $image
                     # Enlarge Image to send to barcode reader to increase accuracy. File stored in memory.
-                    #$BarCodeImage = New-Object 
-                    #  $saveimage.SaveFile("$OutDirectory\temp.jpg")  
+                    #$BarCodeImage = New-Object
+                    #  $saveimage.SaveFile("$OutDirectory\temp.jpg")
                     # $BarCodeImage = Resize-Image  -Image $image -scale 2
-        
+
                     # $Imageconverter = New-Object System.Drawing.ImageConverter
                     # [System.Drawing.Bitmap]$Bitmap = $Imageconverter.ConvertFrom($image.FileData.BinaryData)
 
                     #$image = $null
-                    $barcodes = New-Object System.Collections.ArrayList; 
-            
+                    $barcodes = New-Object System.Collections.ArrayList;
+
                     #[barcodeimaging]::ScanPage([ref]$barcodes, $bitmap, 1000, 2, 1)
                     Write-Verbose "Barcode Count Before: $($barcodes.count)"
                     $barcodes = Read-Barcode -Image $image
                     Write-Verbose "Barcodes :"
-                   
+
                     Write-Verbose "Barcode Count after: $($barcodes.count)"
                     $filename = ""
                     $directory = ""
-                    $barcodes = $barcodes | sort-object -Descending   
+                    $barcodes = $barcodes | sort-object -Descending
                     #Check Barcodes to see if p/n is already in SharePoint Site
                     $SiemensIDs = Get-SiemensLicenseID -PartNumbers $barcodes
                     $blnSiemensSoftwareFound = $false
                     $FileTag = $Null
-                    foreach ($code in $barcodes)
-                    {
+                    foreach ($code in $barcodes) {
 
-                        If ($code.type -eq 'Code39')
-                        {
-                        if ($code.text.StartsWith("S"))
-                        {
-                            $fileName = $code.text
-                            $filename = $filename.replace(" ", "")
-                            "Filename  $filename"
-                        }
-                        else
-                        {
-                            if ($SiemensIDs.ContainsKey($code.text))
-                            {
-                                #
-                                $directory = $code.text
-                                "Dir : $directory"
-                                
-                                $blnSiemensSoftwareFound = $true
+                        If ($code.type -eq 'Code39') {
+                            if ($code.text.StartsWith("S")) {
+                                $fileName = $code.text
+                                $filename = $filename.replace(" ", "")
+                                "Filename  $filename"
                             }
-                            elseif (!$blnSiemensSoftwareFound)
-                            { $directory = $code.text}
+                            else {
+                                if ($SiemensIDs.ContainsKey($code.text)) {
+                                    #
+                                    $directory = $code.text
+                                    "Dir : $directory"
+
+                                    $blnSiemensSoftwareFound = $true
+                                }
+                                elseif (!$blnSiemensSoftwareFound)
+                                { $directory = $code.text }
+                            }
+                        }
+                        if ($code.type -eq 'QR') {
+                            $fileTag = "#$($code.Text)#"
                         }
                     }
-                       if ($code.type -eq 'QR')
-                       {
-                          $fileTag = "#$($code.Text)#"
-                       }            
-                    }
-                   
-                    If (($barcodes.count -ne 2 -and !$blnSiemensSoftwareFound) -or $filename -eq '' -or !$blnSiemensSoftwareFound)
-                    {
+
+                    If (($barcodes.count -ne 2 -and !$blnSiemensSoftwareFound) -or $filename -eq '' -or !$blnSiemensSoftwareFound) {
                         $directory = "Problem\$directory"
-                        $ScanProblem = "" | Select-Object ScanNumber, Directory, FileName, Description 
+                        $ScanProblem = "" | Select-Object ScanNumber, Directory, FileName, Description
                         $ScanProblem.ScanNumber = $scan
-                        $ScanProblem.FileName = "$filename$filetag"
+                        $ScanProblem.FileName = "$filename$filetag".replace("/", "-")
                         $ScanProblem.Directory = $directory
-                        If ($barcodes.count -gt 2 ) 
-                        {   
+                        If ($barcodes.count -gt 2 ) {
                             $ScanProblem.Description = "Too many Barcodes detected.  Please verify "
                         }
-                        If ($filename -eq '' )
-                        {
+                        If ($filename -eq '' ) {
                             $ScanProblem.Description = "Unable to detect Serial Number.  Please Verify"
                         }
-                        If (!$blnSiemensSoftwareFound)
-                        {
+                        If (!$blnSiemensSoftwareFound) {
                             $ScanProblem.Description = "Part Number not located in SharePoint.  Please verify and add to SharePoint."
                         }
                         $problemScan += $ScanProblem
                     }
+                    # Replace invalid character in filename
+                    $directory = $directory.replace("/", "-")
                     #Create Directory if it doesn't exist
-                    if (!(Test-Path $OutDirectory\$Directory))
-                    {
+                    if (!(Test-Path $OutDirectory\$Directory)) {
                         Write-Verbose "Creating $directory"
-                       
+
                         new-item -ItemType Directory -Path $OutDirectory\$directory | out-null
                     }
-                    $index = 1 
+                    $index = 1
                     # If file exists increase index until file doesn't exist
                     $tempFileName = "$filename$FileTag"
-                    While (Test-Path ("$OutDirectory\$directory\$tempfilename.jpg") )
-                    {
-                      
+                    $tempFileName = $tempFileName.replace("/", "-")
+                    While (Test-Path ("$OutDirectory\$directory\$tempfilename.jpg") ) {
+
                         $TempFileName = "$filename-Scan-$index"
                         $index++
                         Write-Verbose "File exists changing file name to $filename"
                     }
                     $Filename = $tempFileName
                     Write-Verbose "Saving File : $OutDirectory\$directory\$filename.jpg"  -Verbose
-                    $saveimage.SaveFile("$OutDirectory\$directory\$filename.jpg")  
+                    $saveimage.SaveFile("$OutDirectory\$directory\$filename.jpg")
                     #  UpdateSiemensLicense -Images "$OutDirectory\$directory\$filename.jpg"
                 }
 
-        
+
             }while ($MorePages)
         }
     }
-    End
-    {
-        If ($problemScan.count -ne 0)
-        {
+    End {
+        If ($problemScan.count -ne 0) {
             Write-host "Detected problems. Please Review"
             $problemScan
         }
